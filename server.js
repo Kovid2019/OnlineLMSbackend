3285c4ce43a056bd1e3b81770592669ab344f208
Kovid Kunduru <kovid@Kovids-MacBook-Air.local>
2025-01-20 13:19:36 +1100

    Backend setup done for Online Library Management System

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//Middlewares
+import cors from "cors";
+import morgan from "morgan";
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+//Server status
+app.get("/", (req, res) => {
+  res.json({ message: "Server is Live" });
+});
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log("Server is running at http://localhost:" + PORT);
+});
